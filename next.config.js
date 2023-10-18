4ed128641cb119648fd691517ca382699e255a30
Tushar Saini <s2sharpit@gmail.com>
2023-10-18 14:09:48 +0530

    adding redirect for '/(cv|resume)'
    Signed-off-by: Tushar Saini <s2sharpit@gmail.com>

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-}
+  async redirects() {
+    return [
+      {
+        source: '/(cv|resume)',
+        destination: "https://drive.google.com/file/d/16Bn8xX8vIYZ4tNV3Axu7T6dLeI4-dXQG/view?usp=sharing",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
