c81b4dbe316d9379aa1e716c0bd15db1daefdb2e
Tushar Saini <s2sharpit@gmail.com>
2023-10-21 14:13:17 +0530

    adding redirect from '/dp'
    Signed-off-by: Tushar Saini <s2sharpit@gmail.com>

--- next.config.js
+++ next.config.js
@@ -10,6 +10,11 @@ const nextConfig = {
         destination: "https://drive.google.com/file/d/16Bn8xX8vIYZ4tNV3Axu7T6dLeI4-dXQG/view?usp=sharing",
         permanent: true,
       },
+      {
+        source: '/dp',
+        destination: "/s2sharpit.png",
+        permanent: true,
+      },
     ];
   },
 };
