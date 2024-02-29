43f707258a2f9815ecccd70c404248ac782678c3
Tushar Saini <s2sharpit@gmail.com>
2024-03-01 01:29:44 +0530

    Add cache revalidation after submitting a new message
    Signed-off-by: Tushar Saini <s2sharpit@gmail.com>

--- src/lib/contactAction.tsx
+++ src/lib/contactAction.tsx
@@ -2,6 +2,7 @@
 
 import { db } from "@/lib/firebaseConfig";
 import { collection, addDoc } from "firebase/firestore";
+import { revalidatePath } from "next/cache";
 
 interface data {
   username: string;
@@ -12,6 +13,7 @@ interface data {
 const submitContact = async (data: data) => {
   try {
     await addDoc(collection(db, "message"), data);
+    revalidatePath("/api/msg"); // revalidate the cache for this page after submitting a new message.
     return { status: 200, message: "Message sent successfully!" };
   } catch (error) {
     return { status: 500, message: "Server error, please try again!" };
