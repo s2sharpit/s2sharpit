9edb8582ec0800c30fc3801c8b26b21da62c9326
Tushar Saini <s2sharpit@gmail.com>
2025-04-23 06:08:33 +0530

    moving to nextjs 15 and tailwind 4

--- src/app/api/projects/route.ts
+++
@@ -1,7 +0,0 @@
-import projectData from "@/data/projectData";
-
-export async function GET(request: Request) {
-  return new Response(JSON.stringify(projectData), {
-    headers: { "Content-Type": "application/json" },
-  })
-}
--- src/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-import Button from "./Button"
-import Scroll from "./Scroll"
-import Section from "./Section"
-import Subtle from "./Subtle"
-import Title from "./Title"
-import Wrapper from "./Wrapper"
-
-export  { Button, Scroll, Section, Subtle, Title, Wrapper };
