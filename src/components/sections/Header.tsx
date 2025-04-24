cfab16ca50c9403e089e6b2f1c456aa058380fbf
Tushar Saini <s2sharpit@gmail.com>
2025-04-24 16:50:15 +0530

    feat: set default theme to light in ThemeProvider; add responsive class to Header button

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -108,6 +108,7 @@ export default function Header() {
         <Button
           variant="outline"
           size='icon'
+          className="max-md:hidden"
           onClick={() => setTheme(theme === "light" ? "dark" : "light")}
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
